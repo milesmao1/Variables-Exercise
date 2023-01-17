66a5761f4aaec3637e1ce02bd36b546bc5b6a1d2
Miles Mao <mrmao1@gmail.com>
2023-01-17 09:53:24 -0600

    variables exercise results

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; // This is declaration syntax
+
+            dogName = "Bo"; // Initializing variable
+
+            int dogAge = 3; // declaring and initializing
+
+            char firstInitial = 'T';
+
+            bool isHungry = true;
+
+            decimal dogHeight = 15.0m;
+
+            double dogWeight = 48.3;
+
+            Console.WriteLine($"My dog's name is {dogName}. He is {dogAge} years old." +
+                $" It is {isHungry} he is always hungry. " +
+                $"He weighs {dogWeight} pounds and is {dogHeight} inches tall.");
+
+
         }
     }
 }
